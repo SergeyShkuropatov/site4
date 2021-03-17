8aea42919674ef50761a5fd71f0b0f910bbd50e5
SergeyShkuropatov <agrro74@mail.ru>
2021-03-17 12:38:22 +0300

    .

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="images/favicon/site.webmanifest">
+    <link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="images/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="images/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+   
+	 <div class="wrapper">
+		<div class="top wrapper-row">
+			<header class="item">
+				<a class="logo" href="#">
+					<img src="/images/logo.png" alt="logo">
+				</a>
+				<nav>
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Blog</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Services</a></li>
+						<li><a href="#">Shop</a></li>
+						<li><a href="#">Contact</a></li>
+				    </ul>
+				</nav>
+			</header>
+			<div class="separator item"></div>
+			<article class="item">
+				<div class="top1"></div>
+				<div class="mask"></div>
+				<div class="info_box">
+					<h2>Inspiration</h2>
+					<p>Shop description</p>
+				</div>
+			</article>
+			<article class="item">
+				<div class="top2"></div>
+				<div class="mask"></div>
+				<div class="info_box">
+					<h2>Inspiration</h2>
+					<p>Shop description</p>
+				</div>
+			</article>
+			<article class="item">
+				<div class="top3"></div>
+				<div class="mask"></div>
+				<div class="info_box">
+					<h2>Shop</h2>
+					<p>Shop description</p>
+				</div>
+			</article>
+		</div>
+		<div class="stats wrapper-row">
+			<div class="item info">
+				<div class="stats1">
+					<span class="stat_col">
+						<span class="number">58</span>
+						<span class="description">Photo collections</span>
+					</span>
+				</div>
+				<div class="stats2">
+					<span class="stat_col">
+						<span class="number">20</span>
+						<span class="description">Photo collections</span>
+					</span>
+				</div>
+				<div class="stats3">
+					<span class="stat_col">
+						<span class="number">61</span>
+						<span class="description">Photo collections</span>
+					</span>
+		        </div>
+				<div class="info_block">
+					<h3>
+						SOCHA. RESPONSIVE THEME
+					</h3>
+					<p>
+						There are many variations of passages of Lorem ipsum
+						available, but the majority have suffered atteration
+						in some form, by injected humour, or randomised words
+						which dont look even slighity believable. If you are
+						going to use a passage of Lorem ipsum
+					</p>
+					<a href="#" class="button">Read More</a>
+				</div>
+			</div>
+			<div class="item video">
+				<div class="wrapper-video">
+					<video controls="controls"> 
+						<source src="images/video_1.ogv" type="video/ogg"> 
+						<source src="images/video_1.mp4" type='video/ogg; codecs="theora, vorbis"'>
+						<source src="images/video_1.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+						<source src="images/video_1.mp4" type='video/webm; codecs="vp8, vorbis"'>   
+					</video>
+					<figure>
+						<figcaption>
+							<span class="video_title">SOCHA <strong>adventures</strong></span>
+							<p>Short video presentation</p>
+						</figcaption> 
+					</figure>
+				</div>
+			</div>
+		</div> 
+		<div class="title wrapper-row">
+			<div class="item dotted_line"></div>
+			<h4 class="item">Collection</h4>
+			<div class="item dotted_line"></div>
+		</div>
+		<div class="gallery wrapper-row">
+			<article class="item">
+				<div class="gal1"></div>
+				<div class="content_box">
+					<h3>Milford Sound</h3>
+					<span>New Zealand</span>
+					<p>
+						There are many variations of passages of Lorem Ipsum available, but
+						the majority have suffered alteration in some form, by injected.
+					</p>
+				</div>
+			</article>
+			<article class="item">
+				<div class="gal2"></div>
+				<div class="content_box">
+					<h3>Milford Sound</h3>
+					<span>New Zealand</span>
+					<p>
+						There are many variations of passages of Lorem Ipsum available, but
+						the majority have suffered alteration in some form, by injected.
+					</p>
+				</div>
+			</article>
+			<article class="item">
+				<div class="gal3"></div>
+				<div class="container">
+					<div class="content_box">
+						<h3>Milford Sound</h3>
+						<span>New Zealand</span>
+					</div>
+				</div>
+			</article>
+			<article class="item">
+				<div class="gal4"></div>
+				<div class="container">
+					<div class="content_box">
+						<h3>Milford Sound</h3>
+						<span>New Zealand</span>
+					</div>
+				</div>
+			</article>
+		</div>
+
+		<footer class="wrapper-row">
+			<p>
+				2013. SOCHA RESPONSIVE THEME. ALL RIGHTS RESERVED.
+			</p>
+			<p>TOP</p>
+		</footer>
+	 </div>
+</body>
+</html>
